a29ec3de2f28e3cf506acdcd6414149f22b051d2
Pavel Kvindt <pakvindt@gmail.com>
2022-08-09 18:17:53 +0800

    add input and button

---
+++ client/src/Form/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../components/Button/Button'
+import Input from '../components/Input/Input'
+
+const Form = () => {
+  return (
+    <div className='flex justify-around'>
+      <Input />
+      <Button />
+    </div>
+  )
+}
+
+export default Form
--- server/prisma/dbConnect.js
+++ server/prisma/dbConnect.js
@@ -1,11 +1,11 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client')
 
-exports.dbconnect = async () => {
-    try {
-      const prisma = new PrismaClient();
-    await prisma.$connect();
-    console.log('Connect to database');
+exports.dbConnect = async () => {
+  try {
+    const prisma = new PrismaClient()
+    await prisma.$connect()
+    console.log('Connect to database')
   } catch (error) {
-    console.log('ебал я призму:',error.message);
+    console.log('ебал я призму:', error.message)
   }
-};
+}
