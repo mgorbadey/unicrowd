952e2f8e5239a7428e224026ea832be293c1ed08
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-08-10 17:49:07 +0300

    reg/login front

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import MasterCalendar from '../../pages/MasterCalendar'
 import Header from '../Header/Header'
+import LoginForm from '../LoginForm/LoginForm'
 import Main from '../Main/Main'
 import MasterProfile from '../MasterProfile/MasterProfile'
+import RegistrationForm from '../RegistrationForm/RegistrationForm'
 
 const Layout = () => {
   return (
@@ -15,6 +17,8 @@ const Layout = () => {
             {/* <Route path='/' element={<Navigate to={} replace={true} />} /> */}
             <Route path='/masters/:id/schedules' element={<MasterCalendar />} />
             <Route path='/masters/:id/profile' element={<MasterProfile />} />
+            <Route path='/auth/login' element={<LoginForm />} />
+            <Route path='/auth/registration' element={<RegistrationForm />} />
           </Routes>
         </Main>
       </div>
