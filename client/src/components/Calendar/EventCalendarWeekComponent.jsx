cfe92f35f6a794dabf6cb8cc5c78a4f7c17a2a73
Никита Сазонов <nikitasazonov@MacBook-Pro-Nikita.local>
2022-08-16 20:27:51 +0800

    null

--- client/src/components/Calendar/EventCalendarWeekComponent.jsx
+++ client/src/components/Calendar/EventCalendarWeekComponent.jsx
@@ -3,6 +3,18 @@ import { Fragment, useState, useEffect } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { useParams } from 'react-router-dom'
 import $api from '../../http/index'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure
+} from '@chakra-ui/react'
+
 
 export default function EventCalendarWeekComponent({ workingSlot }) {
   const HoursCalDisp = {
@@ -11,6 +23,8 @@ export default function EventCalendarWeekComponent({ workingSlot }) {
     endHours: new Date(workingSlot.endDateTime).getHours(),
     endMinutes: String(new Date(workingSlot.endDateTime).getMinutes()).padStart(2, "0"),
   }
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const [openEvent, setOpenEvent] = useState(false)
   const [eventInfo, setEventInfo] = useState([])
@@ -43,7 +57,8 @@ export default function EventCalendarWeekComponent({ workingSlot }) {
   }
 
   const clientCreateEvent = async (id) => {
-    // const userId = JSON.parse(localStorage.getItem('id'))
+    const userId = JSON.parse(localStorage.getItem('user'))
+    console.log(userId)
     // const eventInfo = { masterId: params.id, clientId: userId, serviceItemId: select.id}
 
     // $api.post('/client/event/schedule', eventInfo)
@@ -66,113 +81,38 @@ export default function EventCalendarWeekComponent({ workingSlot }) {
   return (
     <>
       <li
-        className={`relative mt-px flex col-start-${workingSlot.weekDay}`}
+        className={`relative mt-px flex sm:col-start-${workingSlot.weekDay}`}
         style={{ gridRow: `${workingSlot.gridRow} / span ${workingSlot.span}` }}
-        onClick={() => setOpenEvent(true)}
+        onClick={onOpen}
       >
         <a
-          href={`/masters/${workingSlot.masterId}/schedules/${workingSlot.id}`}
-          className='group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-gray-50 p-2 text-xs leading-5 hover:bg-gray-100'
+          href='#'
+          className='group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-blue-50 p-2 text-xs leading-5 hover:bg-pink-100'
         >
-          <p className='order-1 font-semibold text-gray-700'>Рабочий слот</p>
-          <p className='text-gray-500 group-hover:text-gray-700'>
+          <p className='order-1 font-semibold text-pink-700'>Рабочий слот</p>
+          <p className='text-pink-500 group-hover:text-pink-700'>
             <time dateTime={workingSlot.startDateTime}>{HoursCalDisp.startHours}:{HoursCalDisp.startMinutes}-{HoursCalDisp.endHours}:{HoursCalDisp.endMinutes}</time>
           </p>
         </a>
       </li>
 
-      <Transition.Root show={openEvent} as={Fragment}>
-        <Dialog as='div' className='relative z-10' onClose={setOpenEvent}>
-          <Transition.Child
-            as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0'
-            enterTo='opacity-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-          >
-            <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
-          </Transition.Child>
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            {/* <Lorem count={2} /> */}
+          </ModalBody>
 
-          <div className='fixed z-10 inset-0 overflow-y-auto'>
-            <div className='flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0'>
-              <Transition.Child
-                as={Fragment}
-                enter='ease-out duration-300'
-                enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-                enterTo='opacity-100 translate-y-0 sm:scale-100'
-                leave='ease-in duration-200'
-                leaveFrom='opacity-100 translate-y-0 sm:scale-100'
-                leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-              >
-                <Dialog.Panel className='relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6'>
-                  <div>
-                    <div className='mt-3 text-center sm:mt-5'>
-                      <Dialog.Title
-                        as='h3'
-                        className='text-lg leading-6 font-medium text-gray-900'
-                      >
-                        Запись на услуги
-                      </Dialog.Title>
-                      <div className='mt-2 mt-style'>
-                        <label
-                          htmlFor='title'
-                          className='block text-sm font-medium text-gray-700'
-                        >
-                          Название услуги
-                        </label>
-                        <select
-                          name='country'
-                          autoComplete='country-name'
-                          onChange={onChangeHandler}
-                          defaultValue={categoryTitle}
-                          className='max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md'
-                        >
-                          {eventInfo?.data?.eventInfo.map((el) => (
-                            <option key={el.id} id={el.id} value={el.serviceItem.title}>
-                              {el.serviceItem.title}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                      <div className='mt-2 mt-style'>
-                        <label
-                          htmlFor='title'
-                          className='block text-sm font-medium text-gray-700'
-                        >
-                          Время записи
-                        </label>
-                        <select
-                          name='country'
-                          autoComplete='country-name'
-                          onChange={onChangeTime}
-                          className='max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md'
-                        >
-                          {eventInfo?.data?.eventInfo.map((el) => (
-                            <option key={el.id} id={el.startDateTime} value={el.startDateTime}>
-                              {el.startDateTime.slice(11, 16)}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-                  <div className='mt-5 sm:mt-6'>
-                    <button
-                      type='button'
-                      className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm'
-                      onClick={clientCreateEvent}
-                    >
-                      Записаться
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition.Root>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
--- server/src/controllers/masterController.js
+++ server/src/controllers/masterController.js
@@ -50,8 +50,8 @@ exports.getAllClientEvents = async (req, res) => {
   console.log(endDate ,endDateFormatted)
 
 
-  const result = await prisma.$queryRawUnsafe(`select e."id" as "id", e."startDateTime" as "startDateTime", (e."startDateTime" + (si."duration"||' minutes')::interval) as "endDateTime", e."status" as "status", e."clientId" as "clientId", u."username" as "clientName", e."masterId" as "masterId", sc."title" as "serviceCategoryTitle", si."title" as "serviceItemTitle", si."duration" as "serviceItemDuration" from "Event" e left join "ServiceItem" si on e."serviceItemId" = si.id left join "ServiceCategory" sc on sc.id = si."serviceCategoryId" left join "User" u on u.id = e."clientId" where e."masterId" = ${masterId} and e."startDateTime" >= Date('${startDateFormatted}') and (e."startDateTime" + (si."duration"||' minutes')::interval) <= Date('${endDateFormatted}') + (1||' days')::interval`)
-  
+  // const result = await prisma.$queryRawUnsafe(`select e."id" as "id", e."startDateTime" as "startDateTime", (e."startDateTime" + (si."duration"||' minutes')::interval) as "endDateTime", e."status" as "status", e."clientId" as "clientId", u."username" as "clientName", e."masterId" as "masterId", sc."title" as "serviceCategoryTitle", si."title" as "serviceItemTitle", si."duration" as "serviceItemDuration" from "Event" e left join "ServiceItem" si on e."serviceItemId" = si.id left join "ServiceCategory" sc on sc.id = si."serviceCategoryId" left join "User" u on u.id = e."clientId" where e."masterId" = ${masterId} and e."startDateTime" >= Date('${startDateFormatted}') and (e."startDateTime" + (si."duration"||' minutes')::interval) <= Date('${endDateFormatted}') + (1||' days')::interval`)
+  const result = []
   // console.log(result)
 
   let allClientEvents = eventPositionInCal(result)
